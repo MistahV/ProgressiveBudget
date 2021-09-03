2c2d34b0b34c5a812c17e78b863e7223982b640a
chrisvanetten <christophervanetten@gmail.com>
2021-09-03 17:46:48 -0500

    fixed service-worker.js, troubleshooted some bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,21 +12,39 @@ const STATIC_CACHE = `static-cache-v1`;
 const RUNTIME_CACHE = `runtime-cache`;
 
 self.addEventListener('install', (event) => {
+    // event.waitUntil(
+    //     caches
+    //         .open(STATIC_CACHE)
+    //         .then((cache))
+    // )
     event.waitUntil(
-        caches
-            .open(STATIC_CACHE)
-            .then((cache))
+      caches.open(STATIC_CACHE).then(function(cache) {
+        console.log("installing");
+        return cache.addAll(FILES_TO_CACHE)
+      })
     )
 });
 
 self.addEventListener('activate', (event) => {
-    const Caches = [RUNTIME_CACHE, STATIC_CACHE];
     event.waitUntil(
-        caches.keys().then
+      caches.keys().then(function(keyList) {
+        let cacheList = keyList.filter(function (key) {
+          return key.indexOf("static-cache-")
+        })
+
+        cacheList.push(STATIC_CACHE);
+
+        return Promise.all(keyList.map(function (key, i) {
+          if(cacheList.indexOf(key) === -1) {
+            console.log("deleting")
+            return caches.delete(keyList[i])
+          }
+        }))
+      })
     )
 });
 
-self.addEventListenter('fetch', (event) => {
+self.addEventListener('fetch', (event) => {
     if (event.request.url.includes(`/api/transaction`)) {
         event.respondWith(
           caches.open(RUNTIME_CACHE).then((cache) =>
